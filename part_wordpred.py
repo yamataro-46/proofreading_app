0527d700759eaaec5e2cd461aeb6e43049d75a4a
yamataro-46 <yamayama99q@gmail.com>
2023-01-24 15:28:00 +0900

    3rd commit

--- part_wordpred.py
+++ part_wordpred.py
@@ -60,7 +60,7 @@ def regex_txt(text):
 
 # 入力テキストをtoken化
 def text_to_id(texts, vocab_dic):
-    mecab = MeCab.Tagger('-r /dev/null -d /mecab-ipadic-neologd/dicrc')
+    mecab = MeCab.Tagger('-r /dev/null -d /mecab-ipadic-neologd/')
     terms = []
     posList = []
     texts = regex_txt(texts).split('。')
