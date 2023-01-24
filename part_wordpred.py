ce4cde2ff02938b2f075a0d626840a3e372faab8
yamataro-46 <yamayama99q@gmail.com>
2023-01-24 18:31:21 +0900

    v1.0 commit

--- part_wordpred.py
+++ part_wordpred.py
@@ -60,7 +60,8 @@ def regex_txt(text):
 
 # 入力テキストをtoken化
 def text_to_id(texts, vocab_dic):
-    mecab = MeCab.Tagger('-r /dev/null -d /mecab-ipadic-neologd/')
+    mecab = MeCab.Tagger(r'-d "/mecab-ipadic-neologd/"')
+    # '-r /dev/null -d "/mecab-ipadic-neologd/"'
     terms = []
     posList = []
     texts = regex_txt(texts).split('。')
